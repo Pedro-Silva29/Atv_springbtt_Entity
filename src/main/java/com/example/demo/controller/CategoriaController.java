fefa2436a0e9ca6cef8e6ce6af4b1e8fb1677239
MrzDavi <davimarcelo.8@gmail.com>
2024-06-14 19:42:47 -0300

    add categoria controller

--- src/main/java/com/example/demo/controller/CategoriaController.java
+++ src/main/java/com/example/demo/controller/CategoriaController.java
@@ -11,6 +11,41 @@ import java.util.List;
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
+    @Autowired
+    private CategoriaService categoriaServico;
+
+    @GetMapping
+    public List<Categoria> obterTodasCategorias() {
+        return categoriaServico.obterTodasCategorias();
+    }
+
+    @GetMapping("/{categoriaId}")
+    public ResponseEntity<Categoria> obterCategoriaPorId(@PathVariable Long categoriaId) {
+        return categoriaServico.obterCategoriaPorId(categoriaId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Categoria criarCategoria(@RequestBody Categoria categoria) {
+        return categoriaServico.salvarCategoria(categoria);
+    }
+
+    @PutMapping("/{categoriaId}")
+    public ResponseEntity<Categoria> atualizarCategoria(@PathVariable Long categoriaId, @RequestBody Categoria detalhesCategoria) {
+        return categoriaServico.atualizarCategoria(categoriaId, detalhesCategoria)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{categoriaId}")
+    public ResponseEntity<Void> deletarCategoria(@PathVariable Long categoriaId) {
+        if (categoriaServico.deletarCategoria(categoriaId)) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
 
