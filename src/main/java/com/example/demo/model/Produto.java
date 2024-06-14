292dedf860e65db8aff63b8cbaf10085b04a78ec
Marimarinho <mariana.00000847857@unicap.br>
2024-06-14 19:54:48 -0300

    add model category

--- src/main/java/com/example/demo/model/Produto.java
+++ src/main/java/com/example/demo/model/Produto.java
@@ -4,5 +4,72 @@ import jakarta.persistence.*;
 
 @Entity
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private float preco;
+
+    @Column
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    public Produto(Long id, String nome, float preco, String descricao, Categoria categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+        this.categoria = categoria;
+    }
+
+    public Produto() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }
