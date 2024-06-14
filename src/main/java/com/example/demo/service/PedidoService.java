57b46c7be88b3a072790922adfeb34b0d5619e4e
sabrinnawin <sabrina.00000847856@unicap.br>
2024-06-14 19:49:56 -0300

    add service pedido

--- src/main/java/com/example/demo/service/PedidoService.java
+++ src/main/java/com/example/demo/service/PedidoService.java
@@ -10,5 +10,35 @@ import java.util.Optional;
 
 @Service
 public class PedidoService {
+    @Autowired
+    private PedidoRepository pedidoRepositorio;
 
+    public List<Pedido> obterTodosPedidos() {
+        return pedidoRepositorio.findAll();
+    }
+
+    public Optional<Pedido> obterPedidoPorId(Long id) {
+        return pedidoRepositorio.findById(id);
+    }
+
+    public Pedido salvarPedido(Pedido pedido) {
+        return pedidoRepositorio.save(pedido);
+    }
+
+    public Optional<Pedido> atualizarPedido(Long id, Pedido detalhesPedido) {
+        return pedidoRepositorio.findById(id).map(pedido -> {
+            pedido.setEndereco(detalhesPedido.getEndereco());
+            pedido.setValorTotal(detalhesPedido.getValorTotal());
+            pedido.setUsuario(detalhesPedido.getUsuario());
+            pedido.setProdutos(detalhesPedido.getProdutos());
+            return pedidoRepositorio.save(pedido);
+        });
+    }
+
+    public boolean deletarPedido(Long id) {
+        return pedidoRepositorio.findById(id).map(pedido -> {
+            pedidoRepositorio.delete(pedido);
+            return true;
+        }).orElse(false);
+    }
 }
