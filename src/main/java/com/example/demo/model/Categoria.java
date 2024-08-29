e5efb280db3200f611190da564819413654cce4a
Pedro Tavares <pedrotavaresbol1@gmail.com>
2024-08-29 11:18:00 -0300

    import lib jackson in prod/cat

--- src/main/java/com/example/demo/model/Categoria.java
+++ src/main/java/com/example/demo/model/Categoria.java
@@ -3,6 +3,8 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 public class Categoria {
--- src/main/java/com/example/demo/model/Produto.java
+++ src/main/java/com/example/demo/model/Produto.java
@@ -1,6 +1,7 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produto {
